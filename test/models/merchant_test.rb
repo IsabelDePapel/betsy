3955bdd5994c533bb9b2da3b85ea9d2663882f6b
Jessica Owens <vertige@gmail.com>
2017-10-17 16:24:34 -0700

    added belongs to user test

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -2,11 +2,15 @@ require "test_helper"
 
 describe Merchant do
   let(:merchant) { Merchant.new }
+  let(:merchant1) {merchants(:merchant1)}
 
   describe "model relationships" do
     #TODO belongs_to :user
     it "belongs to a user" do
+      merchant1.valid?.must_equal true
 
+      merchant1.user = nil
+      merchant1.valid?.must_equal false
     end
   end #model relationships
 
