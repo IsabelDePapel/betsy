e817a6d948d372014b5593230669a1bae19e7bad
IsabelDePapel <isuchanek@gmail.com>
2017-10-17 16:09:16 -0700

    add order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017224234) do
+ActiveRecord::Schema.define(version: 20171017225655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20171017224234) do
     t.index ["email"], name: "index_merchants_on_email", unique: true
     t.index ["user_id"], name: "index_merchants_on_user_id"
     t.index ["username"], name: "index_merchants_on_username", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +49,5 @@ ActiveRecord::Schema.define(version: 20171017224234) do
   end
 
   add_foreign_key "merchants", "users"
+  add_foreign_key "orders", "users"
 end
