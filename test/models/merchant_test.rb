452bf5e040420dbfa6a31c5dafb62765e3bc5b76
Jessica Owens <vertige@gmail.com>
2017-10-17 16:12:25 -0700

    prep model tests for merchant

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -3,7 +3,15 @@ require "test_helper"
 describe Merchant do
   let(:merchant) { Merchant.new }
 
-  it "must be valid" do
-    value(merchant).must_be :valid?
-  end
+  describe "model relationships" do
+    #TODO belongs_to :user
+    it "belongs to a user" do
+
+    end
+  end #model relationships
+
+  describe "validations" do
+    #TODO validates :username, presence: true, uniqueness: { case_sensitive: false }
+    #TODO validates :email, presence: true, uniqueness: { case_sensitive: false }
+  end #validations
 end
