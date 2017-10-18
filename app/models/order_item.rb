57ef2fc0c6366d79516b53d512e2387871c789f5
IsabelDePapel <isuchanek@gmail.com>
2017-10-17 20:42:31 -0700

    test order-item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,10 @@
 class OrderItem < ApplicationRecord
+  # add cart to status array--track cart orders that way???
+  STATUSES = %w(pending paid complete canceled)
+
   belongs_to :order
   belongs_to :product
 
-  validates :quantity, allow_blank: true, numericality: { greater_than: 0 }
+  validates :status, inclusion: { in: STATUSES}
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,9 +1,77 @@
 require "test_helper"
 
 describe OrderItem do
-  let(:order_item) { OrderItem.new }
+  let(:pending) { order_items(:pending) }
+  let(:paid) { order_items(:paid) }
 
-  it "must be valid" do
-    value(order_item).must_be :valid?
+  describe "validations" do
+    it "must have a quantity to be valid" do
+      pending.valid?.must_equal true
+
+      pending.quantity = nil
+      pending.valid?.must_equal false
+    end
+
+    it "requires quantity to be an integer > 0" do
+      invalid_qtys = [-1, 0, 1.5]
+
+      valid_qtys = [1, 5, 1000]
+
+      invalid_qtys.each do |qty|
+        pending.quantity = qty
+        pending.valid?.must_equal false, "#{qty} isn't a valid quantity"
+      end
+
+      valid_qtys.each do |qty|
+        pending.quantity = qty
+        pending.valid?.must_equal true, "#{qty} is a valid quantity"
+      end
+    end
+
+    # do we want status to be required? we can add a different status
+    # (e.g. cart) that won't display to merchant view
+    it "requires status to be valid" do
+      pending.valid?.must_equal true
+      pending.status = nil
+
+      pending.valid?.must_equal false
+    end
+
+    it "requires status to be either paid, pending, complete, or canceled" do
+      invalid_statuses = ["shipped", "done", ""]
+
+      invalid_statuses.each do |bad_status|
+        pending.status = bad_status
+        pending.valid?.must_equal false
+      end
+
+      OrderItem::STATUSES.each do |status|
+        pending.status = status
+        pending.valid?.must_equal true
+      end
+    end
+
+  end
+
+  describe "relations" do
+    it "must belong to a product" do
+      pending.valid?.must_equal true
+
+      pending.must_respond_to :product
+      pending.product.must_be_kind_of Product
+
+      pending.product = nil
+      pending.valid?.must_equal false
+    end
+
+    it "must belong to an order" do
+      pending.valid?.must_equal true
+
+      pending.must_respond_to :order
+      pending.order.must_be_kind_of Order
+
+      pending.order = nil
+      pending.valid?.must_equal false
+    end
   end
 end
