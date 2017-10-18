902251931c2f47ba4aa6a0f13257d47f69ab92b0
IsabelDePapel <isuchanek@gmail.com>
2017-10-18 12:43:14 -0700

    add billing validations and set up tests

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -2,7 +2,7 @@ class Billing < ApplicationRecord
   belongs_to :user
 
   # set country to default of USA??
-  requires_presence_of :name, :email, :street1, :city, :state_prov, :zip, :country, :ccnum, :ccmonth, :ccyear
+  validates_presence_of :name, :email, :street1, :city, :state_prov, :zip, :country, :ccnum, :ccmonth, :ccyear
 
   validates :ccmonth, inclusion: 1..12
   validates :ccyear, numericality: { only_integer: true }
