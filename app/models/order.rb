7fb04c91d82045d63f82fb42c7b7b9ed6ab4afd1
IsabelDePapel <isuchanek@gmail.com>
2017-10-18 22:53:10 -0700

    add has many through to prod and orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_one :billing
+  has_many :order_items
+  has_many :products, through: :order_items
 
-  # TODO validate that this can only have one billing
+
+  # TODO must have an order item
+  # how do you require > 0 order items when order items depends on order creation?
 end
