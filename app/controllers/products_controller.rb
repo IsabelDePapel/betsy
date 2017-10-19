0269dfc191e1ddd63d7a4c8184cb3b52fe96b4e7
Stef <seedsofthesesame@gmail.com>
2017-10-19 10:17:22 -0700

    Creation of products controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,56 @@
 class ProductsController < ApplicationController
+
   def index
+    @products = Products.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def delete
+    @product = Products.find_by(id: params[:id].to_i)
   end
 
   def new
-  end
-
-  def update
+    @product = Product.new
   end
 
   def create
+    @product = Product.new product_params
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id].to_i)
+
+    unless @product
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id].to_i)
+    redirect_to products_path unless @product
+
+    if @product.update_attributes product_params
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Product.find_by(id: params[:id])
+    @product = Product.find_by
+    if @product == nil
+      redirect_to root_path
+    else @product.destroy
+    end
+  end
+
+  private
+  def product_params
+    return params.require(:product).permit(:category, :price, :quantity, :name, :description)
   end
 end
