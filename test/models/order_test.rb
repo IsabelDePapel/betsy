cbc925e8c6f38e284d3b642b66e036d9d350ea26
IsabelDePapel <isuchanek@gmail.com>
2017-10-19 09:32:20 -0700

    add third order item and more tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,8 +2,14 @@ require "test_helper"
 
 describe Order do
   let(:order) { orders(:one) }
+  let(:completed_order) { orders(:three) }
 
-  it "must be valid" do
-    value(order).must_be :valid?
+  describe "relations" do
+    it "can access order_items and its data" do
+      completed_order_item = order_items(:complete)
+      completed_order.order_items.must_equal completed_order_item
+
+    end
   end
+
 end
