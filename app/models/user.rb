4b25dc837e5a25bdf429d99d66c8ed45ddfadfd5
Jessica Owens <vertige@gmail.com>
2017-10-19 14:27:56 -0700

    Added merchant tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_one :merchant
+  has_one :merchant, dependent: :nullify
 
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -21,17 +21,30 @@ describe Merchant do
         merchant1.valid?.must_equal false
       end
 
-      it "can access its user data and modify it" do
-        merchant1.user.must_be_kind_of User
+      it "can access its user data" do
         an_id = merchant1.user.id
         User.find(an_id).must_equal merchant1.user
+      end
+    end
 
-        # merchant1.user.email = "new@email.com"
+    describe "has many products" do
+      it "can have 0 or many products and can list its products" do
+        puts merchant1.products
+        merchant1.products.length.must_equal 2
+        merchant1.products.must_include products(:croissant) && products(:macaroon)
+
+        products.destroy_all
+        merchant1.valid?.must_equal true
+        merchant1.products.must_equal []
       end
 
+      it "can access its products data" do
+        merchant1.products << cupcake
+        merchant1.products.first.name.must_equal "Chocolate Peanut Butter Cupcake"
+      end
     end
   end #model relationships
-#
+  #
   describe "validations" do
     #TODO validates :username, presence: true, uniqueness: { case_sensitive: false }
     #TODO validates :email, presence: true, uniqueness: { case_sensitive: false }
